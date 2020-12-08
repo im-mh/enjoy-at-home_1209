bb9aaa9b4077f3135ec40182cb08d1094a57ac4b
im-mh <62644451+im-mh@users.noreply.github.com>
2020-12-09 04:14:04 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+	$(".appear1").on("click", function () {
+		$(".movies").animate({opacity:'100'}, 600)
+	})
+
+	$(".appear2").on("click", function () {
+		$(".homebar").animate({opacity:'100'}, 600)	
+	})
+
+	$(".appear3").on("click", function () {
+		$(".exercise").animate({opacity:'100'}, 600)
+	})
+
+	$(".appear4").on("click", function () {
+		$(".sunrise").animate({opacity:'100'}, 600)	
+	})
+
+	$(".appear5").on("click", function () {
+		$(".homecafe").animate({opacity:'100'}, 600)		
+	})
+
+	$(".appear6").on("click", function () {
+		$(".plants").animate({opacity:'100'}, 600)
+	})
+
+})
